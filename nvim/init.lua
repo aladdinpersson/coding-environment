6295b8cf19d1bc11f5f4120e232352f310e7f840
Aladdin Persson <aladdin.persson@hotmail.com>
2023-11-30 16:07:03 +0100

    update initlua with persistent undo

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,14 @@ require("mrbean.plugins.comment")
 require("mrbean.plugins.nvim-tree")
 require("mrbean.plugins.lualine")
 require("mrbean.plugins.telescope")
+
+local undodir = vim.fn.expand('~/.undodir')
+
+-- Create the undo directory if it doesn't exist
+if vim.fn.isdirectory(undodir) == 0 then
+  os.execute('mkdir -p ' .. undodir)
+end
+
+-- Set the undodir and enable undofile
+vim.opt.undodir = undodir
+vim.opt.undofile = true
